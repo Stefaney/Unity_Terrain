21131ee8b917e268f16dc77650ac222d540978a3
Stefaney <sfg239@nyu.edu>
2014-07-15 10:04:36 -0400

    this is the commit

---
+++ Week01c/Terrain/Assets/TextScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//Debug.Log("Hello, World!");
+
+		GetComponent<TextMesh> ().text = "“Oh that the desert were my dwelling place,\n With only one fair spirit for my minster.\n " +
+			"That I might forget the human race\n And hating no one, love her only.”\n -Lord Byron";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
